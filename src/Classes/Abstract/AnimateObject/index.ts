b13c0e146797c9c23fadab344bde8f9f14277b32
Kia Farhang <kfarhang0@gmail.com>
2017-09-03 15:46:11 -0500

    Added method to check move legality on AnimateObject abstract class

--- src/Classes/Abstract/AnimateObject/index.ts
+++ src/Classes/Abstract/AnimateObject/index.ts
@@ -1,24 +1,31 @@
 import { GameObject } from '../GameObject';
 import { Coordinates } from '../../Concrete/Coordinates';
+import { KeyInput } from '../../../Interfaces/KeyInput';
 
-export abstract class AnimateObject extends GameObject{
+export abstract class AnimateObject extends GameObject {
 	protected charCode: string;
 	protected location: Coordinates;
 
-	constructor(location: Coordinates){
+	constructor(location: Coordinates) {
 		super();
 		this.location = location;
 	}
 
-	public getLocation(): Coordinates{
+	public getLocation(): Coordinates {
 		return this.location;
 	}
 
-	protected setLocation(newLocation: Coordinates): void{
+	protected setLocation(newLocation: Coordinates): void {
 		this.location = newLocation;
 	}
 
-	public abstract move(maxRow: number, maxColumn: number): Coordinates;
+	protected isMoveLegal(maxRow: number, maxColumn: number, destination: Coordinates): boolean {
+		if (destination.getX() < 0 || destination.getY() < 0) {
+			return false;
+		}
 
+		return destination.getX() <= maxRow && destination.getY() <= maxColumn;
+	}
 
+	public abstract move(maxRow: number, maxColumn: number, key?: KeyInput): Coordinates;
 }
