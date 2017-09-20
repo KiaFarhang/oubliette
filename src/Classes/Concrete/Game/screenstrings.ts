3f7cf45270d03c86d4feffb99b95d9fdf95b6a6b
Kia Farhang <kfarhang0@gmail.com>
2017-09-19 19:31:21 -0500

    Added strings representing various game strings (title, game over, beat level)

---
+++ src/Classes/Concrete/Game/screenstrings.ts
@@ -0,0 +1,5 @@
+export const startScreenString: string = `Welcome to Oubliette.\n\nYou (L) are trapped deep in a dungeon with a hungry dragon (S).\n\nYou must reach the door (@) to move to the next level.\n\nMove through all the doors and you will see daylight.\n\nUse the arrow keys to move.\n\nPress 'q' at any time to quit.\n\nPress space to begin.`;
+
+export const loseString: string = 'A dragon ate you.\n\nYou are dead.\n\nPlay again?\n\n(Press space to play again, or q to quit.)';
+
+export const winString: string = 'You reached the door and escaped the dragon!\n\nPress space to move to the next level, or q to quit.';
