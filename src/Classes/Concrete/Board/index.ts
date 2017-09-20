81ebc048d20fa1aea2c67479fcb86b92beb670ad
Kia Farhang <kfarhang0@gmail.com>
2017-09-19 20:15:31 -0500

    Added optional key paramater to Board's moveAnimateObjects method

--- src/Classes/Concrete/Board/index.ts
+++ src/Classes/Concrete/Board/index.ts
@@ -1,4 +1,3 @@
-import { BoardConfig } from '../../../Interfaces/BoardConfig';
 import { GameObject } from '../../Abstract/GameObject';
 import { Coordinates } from '../Coordinates';
 import { Door } from '../Door';
@@ -6,7 +5,10 @@ import { Space } from '../Space';
 import { Player } from '../Player';
 import { Dragon } from '../Dragon';
 
-export class Board implements BoardConfig{
+import { BoardConfig } from '../../../Interfaces/BoardConfig';
+import { KeyInput } from '../../../Interfaces/KeyInput';
+
+export class Board implements BoardConfig {
 	rows: number;
 	columns: number;
 	playerStartLocation: Coordinates;
@@ -16,7 +18,7 @@ export class Board implements BoardConfig{
 	private player: Player;
 	private dragon: Dragon;
 
-	constructor(config: BoardConfig){
+	constructor(config: BoardConfig) {
 		this.rows = config.rows;
 		this.columns = config.columns;
 		this.playerStartLocation = config.playerStartLocation;
@@ -29,14 +31,14 @@ export class Board implements BoardConfig{
 		this.placeObjects();
 	}
 
-	public draw(): string{
+	public draw(): string {
 		let stringToReturn: string = '';
 
-		for(let i: number = 0; i < this.rows; i++){
-			for(let j: number = 0; j < this.columns; j++){
+		for (let i: number = 0; i < this.rows; i++) {
+			for (let j: number = 0; j < this.columns; j++) {
 				const inhabitant: GameObject = this.sites[i][j];
 				stringToReturn += inhabitant.getCharCode();
-				if (j === this.columns - 1){
+				if (j === this.columns - 1) {
 					stringToReturn += '\n';
 				}
 			}
@@ -45,55 +47,54 @@ export class Board implements BoardConfig{
 		return stringToReturn;
 	}
 
-	public moveAnimateObjects(): void{
+	public moveAnimateObjects(key?: KeyInput): void {
 		const maxRow: number = this.rows - 1;
 		const maxColumn: number = this.columns - 1;
-		
+
 		const oldDragonSpace: Coordinates = this.dragon.getLocation();
 		const oldPlayerSpace: Coordinates = this.player.getLocation();
-		
-		const dragonDestination: Coordinates = this.dragon.move(maxRow, maxColumn);
-		const playerDestination: Coordinates = this.player.move(maxRow, maxColumn);
 
-		if (!Coordinates.equal(oldPlayerSpace, playerDestination)){
+		const dragonDestination: Coordinates = this.dragon.move(maxRow, maxColumn);
+		const playerDestination: Coordinates = this.player.move(maxRow, maxColumn, key);
+		if (!Coordinates.equal(oldPlayerSpace, playerDestination)) {
 			this.updateSite(playerDestination, this.player);
 			this.updateSite(oldPlayerSpace, new Space());
 		}
 
-		if (!Coordinates.equal(oldDragonSpace, dragonDestination)){
+		if (!Coordinates.equal(oldDragonSpace, dragonDestination)) {
 			this.updateSite(dragonDestination, this.dragon);
 
 			//If dragon space matches door space, we need to re-add the door after dragon moves
-			if (Coordinates.equal(oldDragonSpace, this.doorLocation)){
+			if (Coordinates.equal(oldDragonSpace, this.doorLocation)) {
 				this.updateSite(oldDragonSpace, new Door());
-			}else{
+			} else {
 				this.updateSite(oldDragonSpace, new Space());
 			}
 		}
 	}
 
-	public getPlayerLocation(): Coordinates{
+	public getPlayerLocation(): Coordinates {
 		return this.player.getLocation();
 	}
 
-	public getDragonLocation(): Coordinates{
+	public getDragonLocation(): Coordinates {
 		return this.dragon.getLocation();
 	}
 
-	public isPlayerAtDragon(): boolean{
+	public isPlayerAtDragon(): boolean {
 		return Coordinates.equal(this.player.getLocation(), this.dragon.getLocation());
 	}
 
-	public isPlayerAtDoor(): boolean{
+	public isPlayerAtDoor(): boolean {
 		return Coordinates.equal(this.player.getLocation(), this.doorLocation);
 	}
 
-	private constructBoard(): void{
+	private constructBoard(): void {
 		let array: any[] = [];
 
-		for(let i: number = 0; i < this.rows; i++){
+		for (let i: number = 0; i < this.rows; i++) {
 			array.push([]);
-			for(let j: number = 0; j < this.columns; j++){
+			for (let j: number = 0; j < this.columns; j++) {
 				array[i][j] = new Space();
 			}
 		}
@@ -101,13 +102,13 @@ export class Board implements BoardConfig{
 		this.sites = array;
 	}
 
-	private placeObjects(): void{
+	private placeObjects(): void {
 		this.updateSite(this.playerStartLocation, this.player);
 		this.updateSite(this.dragonStartLocation, this.dragon);
 		this.updateSite(this.doorLocation, new Door());
 	}
 
-	private updateSite(site: Coordinates, newResident: GameObject): void{
+	private updateSite(site: Coordinates, newResident: GameObject): void {
 		const row: number = site.getX();
 		const column: number = site.getY();
 
